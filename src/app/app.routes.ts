17499e4d0ac930bfc0f6d0d55a8805af532b5193
Thomas Reynoso Cordero <thomas.reynoso@externos.w2m.com>
2025-10-26 20:49:58 +0100

    add page structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home').then(m => m.Home)
+  },
+  {
+    path: 'quien-soy',
+    loadComponent: () => import('./features/about/about').then(m => m.About)
+  },
+  {
+    path: 'que-es-crianza-consciente',
+    loadComponent: () => import('./features/what-is/what-is').then(m => m.WhatIs)
+  },
+  {
+    path: 'servicios',
+    loadComponent: () => import('./features/services/services').then(m => m.Services)
+  },
+  {
+    path: 'contacto',
+    loadComponent: () => import('./features/contact/contact').then(m => m.Contact)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ src/app/features/what-is/what-is.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-what-is',
+  imports: [RouterLink],
+  templateUrl: './what-is.html',
+  styleUrl: './what-is.scss',
+})
+export class WhatIs {
+
+}
